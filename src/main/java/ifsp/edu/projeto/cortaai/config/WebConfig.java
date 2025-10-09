4fa72606566dde6cb974cac0d292ae756236ddec
Ana Clara <vieira.clara@aluno.ifsp.edu.br>
2025-10-08 22:47:33 -0300

    permissao de web pro front

---
+++ src/main/java/ifsp/edu/projeto/cortaai/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ifsp.edu.projeto.cortaai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Permite CORS para todos os endpoints em /api
+                .allowedOrigins("http://localhost:8081") // A origem do seu front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
