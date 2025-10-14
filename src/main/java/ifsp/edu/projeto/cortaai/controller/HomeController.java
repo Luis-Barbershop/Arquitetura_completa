06e74f954ee034a462260bcf09e0987a565c59d6
Guilherme Chaves <53453346+gbschaves@users.noreply.github.com>
2025-10-14 02:01:48 -0300

    Update HomeController.java

--- src/main/java/ifsp/edu/projeto/cortaai/controller/HomeController.java
+++ src/main/java/ifsp/edu/projeto/cortaai/controller/HomeController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index() {
-        return "\"Hello World!\"";
+        return "redirect:/swagger-ui/index.html";
     }
 
 }
