f7166bf5be9e597834f48c1255e4304e1d8db6e9
Guilherme Chaves <53453346+gbschaves@users.noreply.github.com>
2025-10-14 01:04:44 -0300

    Update Login.js

--- meuApp/Login.js
+++ meuApp/Login.js
@@ -23,7 +23,7 @@ export default function Login() {
         // --- SUA LÓGICA FETCH (ADAPTADA) ---
         try {
             // O endpoint que você criou no backend é /api/customers/login
-            const response = await fetch('3.224.37.166/api/customers/login', {
+            const response = await fetch('https://api.cortaai.oneaction.space//api/customers/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password })
