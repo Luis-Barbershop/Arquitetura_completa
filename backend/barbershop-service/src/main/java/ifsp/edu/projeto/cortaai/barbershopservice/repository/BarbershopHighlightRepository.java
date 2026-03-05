70eb6846d02507831e3d1c9ab3c0e8c04d2d6952
Gitocy <motta.joao@aluno.ifsp.edu.br>
2026-03-05 13:12:37 -0300

    refactor: barbershop highlight em JPQL

--- backend/barbershop-service/src/main/java/ifsp/edu/projeto/cortaai/barbershopservice/repository/BarbershopHighlightRepository.java
+++ backend/barbershop-service/src/main/java/ifsp/edu/projeto/cortaai/barbershopservice/repository/BarbershopHighlightRepository.java
@@ -2,11 +2,15 @@ package ifsp.edu.projeto.cortaai.barbershopservice.repository;
 
 import ifsp.edu.projeto.cortaai.barbershopservice.model.BarbershopHighlight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface BarbershopHighlightRepository extends JpaRepository<BarbershopHighlight, UUID> {
-    List<BarbershopHighlight> findByBarbershopId(UUID barbershopId);
-}
 
+    @Query("SELECT bh FROM BarbershopHighlight bh WHERE bh.barbershop.id = :barbershopId")
+    List<BarbershopHighlight> findByBarbershopId(@Param("barbershopId") UUID barbershopId);
+    
+};
