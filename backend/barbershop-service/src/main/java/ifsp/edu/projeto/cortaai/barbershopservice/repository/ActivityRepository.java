5268a8eaf584b252add6e2ed4b867f28d04f6f78
Gitocy <motta.joao@aluno.ifsp.edu.br>
2026-03-05 12:26:12 -0300

    feat: transformar findByBarbershop em query jpql

--- backend/barbershop-service/src/main/java/ifsp/edu/projeto/cortaai/barbershopservice/repository/ActivityRepository.java
+++ backend/barbershop-service/src/main/java/ifsp/edu/projeto/cortaai/barbershopservice/repository/ActivityRepository.java
@@ -2,11 +2,15 @@ package ifsp.edu.projeto.cortaai.barbershopservice.repository;
 
 import ifsp.edu.projeto.cortaai.barbershopservice.model.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface ActivityRepository extends JpaRepository<Activity, UUID> {
-    List<Activity> findByBarbershopId(UUID barbershopId);
-}
 
+    @Query("SELECT a FROM Activity a WHERE a.barbershop.id = :barbershopId")
+    List<Activity> findByBarbershopId(@Param("barbershopId") UUID barbershopId);
+    
+}
