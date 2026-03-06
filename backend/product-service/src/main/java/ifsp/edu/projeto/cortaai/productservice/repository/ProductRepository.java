c401292e11c6b75c063a41685fa941219abd404a
Gitocy <motta.joao@aluno.ifsp.edu.br>
2026-03-06 09:00:51 -0300

    refactor: Product JPQL

--- backend/product-service/src/main/java/ifsp/edu/projeto/cortaai/productservice/repository/ProductRepository.java
+++ backend/product-service/src/main/java/ifsp/edu/projeto/cortaai/productservice/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package ifsp.edu.projeto.cortaai.productservice.repository;
 
 import ifsp.edu.projeto.cortaai.productservice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,9 @@ import java.util.UUID;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
-    List<Product> findByBarbershopIdAndActiveTrue(UUID barbershopId);
+    @Query("SELECT p FROM Product p WHERE p.barbershopId = :barbershopId AND p.active = true")
+    List<Product> findByBarbershopIdAndActiveTrue(@Param("barbershopId") UUID barbershopId);
 
-    List<Product> findByBarbershopId(UUID barbershopId);
+    @Query("SELECT p FROM Product p WHERE p.barbershopId = :barbershopId")
+    List<Product> findByBarbershopId(@Param("barbershopId") UUID barbershopId);
 }
