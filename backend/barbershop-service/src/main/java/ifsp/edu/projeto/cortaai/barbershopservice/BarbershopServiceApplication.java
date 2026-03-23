3ff5a0c6c39f549e3368f92bd2a328ffd5941885
Guilherme Chaves <chaves.bsilva@gmail.com>
2026-03-23 00:23:24 -0300

    fix: remove @ComponentScan override from BarbershopServiceApplication (was breaking controller registration and SecurityConfig)

--- backend/barbershop-service/src/main/java/ifsp/edu/projeto/cortaai/barbershopservice/BarbershopServiceApplication.java
+++ backend/barbershop-service/src/main/java/ifsp/edu/projeto/cortaai/barbershopservice/BarbershopServiceApplication.java
@@ -3,11 +3,9 @@ package ifsp.edu.projeto.cortaai.barbershopservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
-@ComponentScan(basePackages = "ifsp.edu.projeto.cortaai") 
 public class BarbershopServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BarbershopServiceApplication.class, args);
