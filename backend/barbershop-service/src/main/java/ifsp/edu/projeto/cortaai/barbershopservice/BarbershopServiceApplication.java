98e28be1acafc4033dd1505b6e8b4eaf4efee6af
Guilherme Chaves <chaves.bsilva@gmail.com>
2026-03-22 22:43:55 -0300

    Ajustando beans de barber

--- backend/barbershop-service/src/main/java/ifsp/edu/projeto/cortaai/barbershopservice/BarbershopServiceApplication.java
+++ backend/barbershop-service/src/main/java/ifsp/edu/projeto/cortaai/barbershopservice/BarbershopServiceApplication.java
@@ -3,14 +3,13 @@ package ifsp.edu.projeto.cortaai.barbershopservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableJpaAuditing
+@ComponentScan(basePackages = "ifsp.edu.projeto.cortaai") 
 public class BarbershopServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BarbershopServiceApplication.class, args);
     }
 }
-
